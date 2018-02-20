9c1e253b3f03bb2a948de668cd26753d9a9f944a
mrpeny <bencekonya@gmail.com>
2018-02-20 12:18:17 +0100

    feat: Implement NetworkUtils class for TMDB Web API queries

--- app/src/main/java/eu/captaincode/popularmovies/MainActivity.java
+++ app/src/main/java/eu/captaincode/popularmovies/MainActivity.java
@@ -1,13 +1,80 @@
 package eu.captaincode.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URL;
+
+import eu.captaincode.popularmovies.utilities.NetworkUtils;
+
+/**
+ * Makes testing on app components in this development phase (MrPeny 2018.02.20.)
+ */
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String> {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.main_iv);
+
+        getSupportLoaderManager().initLoader(42, null, this);
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new MovieLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String jsonString) {
+        if (jsonString == null) {
+            Toast.makeText(this, "Network Error has occured", Toast.LENGTH_SHORT).show();
+        }
+        tv.setText(jsonString);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+    static class MovieLoader extends AsyncTaskLoader<String> {
+        MovieLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onStartLoading() {
+            forceLoad();
+        }
+
+        @Nullable
+        @Override
+        public String loadInBackground() {
+            URL url = NetworkUtils.getMovieDetailsQueryURL(getContext(), "211672");
+            String jsonString = null;
+            try {
+                jsonString = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                Log.d("MainActivity", e.getMessage());
+                e.printStackTrace();
+            }
+            return jsonString;
+        }
     }
 }
---
+++ app/src/main/java/eu/captaincode/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,121 @@
+package eu.captaincode.popularmovies.utilities;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import eu.captaincode.popularmovies.BuildConfig;
+
+/**
+ * Makes network calls to The Movie DB web api.
+ */
+
+public class NetworkUtils {
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String BASE_URL_TMDB = "https://api.themoviedb.org/3";
+    private static final String MOVIE_POPULAR_ENDPOINT_TMDB = "movie/popular";
+    private static final String MOVIE_TOP_RATED_ENDPOINT_TMDB = "movie/top_rated";
+    private static final String MOVIE_DETAILS_ENDPOINT_TMDB = "movie";
+
+    private static final String API_KEY_PARAM_TMDB = "api_key";
+    private static final String LANGUAGE_PARAM_TMDB = "language";
+
+    private static final String DEFAULT_LANGUAGE_TAG_TMDB = "en-US";
+
+
+    /**
+     * Retrieves the URL for The Movie DB based on the user's sorting preferences.
+     *
+     * @param context the application context used for accessing application wide information
+     * @return the generated {@link URL} based on user's preference
+     */
+    public static URL getMovieListQueryUrl(Context context) {
+        Uri tmdbUri = buildBaseUriWithLanguageAndApi();
+
+        boolean popularSorted = true;
+        if (popularSorted) {
+            String moviePopularEndpoint = Uri.decode(MOVIE_POPULAR_ENDPOINT_TMDB);
+            tmdbUri = tmdbUri.buildUpon().appendEncodedPath(moviePopularEndpoint).build();
+        } else {
+            String movieTopRatedEndpoint = Uri.decode(MOVIE_TOP_RATED_ENDPOINT_TMDB);
+            tmdbUri = tmdbUri.buildUpon().appendEncodedPath(movieTopRatedEndpoint).build();
+        }
+        return buildUrlFrom(tmdbUri);
+    }
+
+    /**
+     * Retrieves the URL for the details on a Movie of The Movie DB to based on the given ID.
+     *
+     * @param context the application context used for accessing application wide information
+     * @param movieId the TMDB web API "id" of the movie to get details about
+     * @return the generated {@link URL} based on user's preference
+     */
+    public static URL getMovieDetailsQueryURL(Context context, String movieId) {
+        Uri tmdbUri = buildBaseUriWithLanguageAndApi();
+        tmdbUri = tmdbUri.buildUpon().appendEncodedPath(MOVIE_DETAILS_ENDPOINT_TMDB)
+                .appendEncodedPath(movieId).build();
+        return buildUrlFrom(tmdbUri);
+    }
+
+    /**
+     * Builds up https URLConnection for the given URL and returns the response received from the
+     * server based on the url argument.
+     *
+     * @param url the url containing the query that will be requested from the server
+     * @return the response from the server based on the given url
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        // TODO: Create Network Security Config for https connection {@see https://developer.android.com/training/articles/security-config.html}
+        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            String response = null;
+            if (hasInput) {
+                response = scanner.next();
+            }
+            scanner.close();
+            return response;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    /* Helper methods for Uri and URL builds */
+    private static Uri buildBaseUriWithLanguageAndApi() {
+        String languageTag = DEFAULT_LANGUAGE_TAG_TMDB;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            languageTag = Locale.getDefault().toLanguageTag();
+        }
+        return Uri.parse(BASE_URL_TMDB).buildUpon()
+                .appendQueryParameter(LANGUAGE_PARAM_TMDB, languageTag)
+                .appendQueryParameter(API_KEY_PARAM_TMDB, BuildConfig.API_KEY)
+                .build();
+    }
+
+    private static URL buildUrlFrom(Uri uri) {
+        try {
+            URL queryUrl = new URL(uri.toString());
+            Log.d(TAG, queryUrl.toString());
+            return queryUrl;
+        } catch (MalformedURLException e) {
+            Log.d(TAG, e.getMessage());
+            return null;
+        }
+    }
+}
